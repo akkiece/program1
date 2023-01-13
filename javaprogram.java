d73bc6393eaa77d2ff76fe8bad994744d295c370
mounika <akkiece123@gmail.com>
2023-01-13 11:41:58 +0530

    first commit

---
+++ javaprogram.java
@@ -0,0 +1,15 @@
+package Assignment;
+
+public class javaprogram
+{
+
+	public static void main(String[] args) 
+	{
+		javaprogram p1 = new javaprogram();
+		p1.program1();
+	}
+	public void program1()
+	{
+		System.out.println("This is a simple single program using java");
+	}
+}
